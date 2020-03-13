b945eef98967b793a962cec47d6d71dbffdc76c1
Laura Collins <laura.e.collins1@gmail.com>
2020-03-13 13:35:40 -0500

    Add project files.

---
+++ LunchBuddies/LunchBuddy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchBuddies
+{
+    class LunchBuddy
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
